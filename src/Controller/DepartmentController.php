450424bc994a31058f53c5e7e66ade60c455a1bd
mjf <jf.michael.pik@gmail.com>
2022-10-31 11:53:46 +0100

    implement API Controllers

---
+++ src/Controller/DepartmentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\employee_management\Controller;
+
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\employee_management\Entity\DepartmentInterface;
+use Drupal\employee_management\HistoryManagerInterface;
+use Drupal\employee_management\RestCallbackHandlerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class DepartmentController.
+ *
+ *  Returns responses for Department routes.
+ */
+class DepartmentController extends ControllerBase implements ContainerInjectionInterface {
+  /**
+   * @var RestCallbackHandlerInterface
+   */
+  protected $callbackHandler;
+  /**
+   * @var HistoryManagerInterface
+   */
+  protected $historyManager;
+
+  /**
+   * @param RestCallbackHandlerInterface $callback_handler
+   * @param HistoryManagerInterface $history_manager
+   */
+  public function __construct(RestCallbackHandlerInterface $callback_handler , HistoryManagerInterface $history_manager){
+    $this->callbackHandler = $callback_handler;
+    $this->historyManager = $history_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('api.handler'),
+      $container->get('event_history.manager')
+    );
+  }
+
+  /**
+   * @param $department
+   * @param Request $request
+   * @return JsonResponse
+   */
+  public function getDepartment($department = NULL, Request $request){
+    $response = new JsonResponse();
+    $entity = $this->callbackHandler->getDepartmentById($department);
+    $response->setStatusCode($entity ? 200 : 204);
+    if($entity){
+      $response->setData($entity->toArray(TRUE));
+    }
+    return $response;
+  }
+
+  /**
+   * @param Request $request
+   * @return JsonResponse
+   */
+  public function getDepartmentList(Request $request){
+    $response = new JsonResponse();
+    $departments = $this->callbackHandler->getDepartments();
+    $response->setStatusCode($departments ? 200 : 204);
+    if($departments){
+      $response->setData([
+        'numfound' => count($departments),
+        'entries' => array_map(function (DepartmentInterface $department) {
+          return $department->toArray();
+        },$departments)]);
+    }
+    return $response;
+  }
+}
---
+++ src/Controller/EmployeeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\employee_management\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\employee_management\Entity\EmployeeInterface;
+use Drupal\employee_management\HistoryManagerInterface;
+use Drupal\employee_management\RestCallbackHandlerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class EmployeeController.
+ *
+ *  Returns responses for Employee routes.
+ */
+class EmployeeController extends ControllerBase implements ContainerInjectionInterface {
+  /**
+   * @var RestCallbackHandlerInterface
+   */
+  protected $callbackHandler;
+  /**
+   * @var HistoryManagerInterface
+   */
+  protected $historyManager;
+
+  /**
+   * @param RestCallbackHandlerInterface $callback_handler
+   * @param HistoryManagerInterface $history_manager
+   */
+  public function __construct(RestCallbackHandlerInterface $callback_handler , HistoryManagerInterface $history_manager){
+    $this->callbackHandler = $callback_handler;
+    $this->historyManager = $history_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('api.handler'),
+      $container->get('event_history.manager')
+    );
+  }
+
+  public function getEmployee($employee = NULL, Request $request){
+    $response = new JsonResponse();
+    $entity = $this->callbackHandler->getEmployeeById($employee);
+    $response->setStatusCode($entity ? 200 : 204);
+    if($entity){
+      $response->setData($entity->toArray(TRUE));
+    }
+    return $response;
+  }
+
+  public function getEmployeeList(Request $request){
+    $response = new JsonResponse();
+    $employees = $this->callbackHandler->getEmployees();
+    $response->setStatusCode($employees ? 200 : 204);
+    if($employees){
+      $response->setData([
+        'numfound' => count($employees),
+        'entries' => array_map(function (EmployeeInterface $employee) {
+          return $employee->toArray();
+        },$employees)]);
+    }
+    return $response;
+  }
+}
