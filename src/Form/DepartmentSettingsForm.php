37ec865a6b9c5cfdccc68a56f27d63f11b784720
mjf <jf.michael.pik@gmail.com>
2022-10-31 14:29:43 +0100

    entities default form

---
+++ src/Form/DepartmentSettingsForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\employee_management\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class DepartmentSettingsForm.
+ *
+ * @ingroup employee_management
+ */
+class DepartmentSettingsForm extends FormBase {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'department_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+  /**
+   * Defines the settings form for Department entities.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['department_settings']['#markup'] = 'Settings form for Department entities. Manage field settings here.';
+    return $form;
+  }
+
+}
